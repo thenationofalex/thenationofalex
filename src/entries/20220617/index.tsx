b409b619b3d4c7ae1d6877ebc50a37b6105358d3
Alex B <alexb@thenationofalex.com>
2022-06-17 23:54:31 +1000

    initial

---
+++ src/entries/20220617/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+import styles from '../../../styles/Home.module.css'
+
+const Entry20220617 = () => <>
+  <figure className={styles.imageEntry}>
+    <Image src='/001033720018.jpg' alt='' width='800' height='530' />
+    <figcaption>Flowers, Adelaide 2022</figcaption>
+  </figure>
+</>
+
+export default Entry20220617
