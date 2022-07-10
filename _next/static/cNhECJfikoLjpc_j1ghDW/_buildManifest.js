b13549b8217d7fbbf44b6d53e05cc86622c5d48d
Alex B <alexb@thenationofalex.com>
2022-07-10 22:25:13 +1000

    Deploy

---
+++ _next/static/cNhECJfikoLjpc_j1ghDW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/75cd51a22f9fe8be.css","static/chunks/pages/index-1a6b76015e07efbc.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
