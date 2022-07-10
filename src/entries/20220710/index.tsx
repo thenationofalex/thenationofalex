2326be3cd8759aec9e7d04a4cf54a0e9ad0859d6
Alex B <alexb@thenationofalex.com>
2022-07-10 22:25:03 +1000

    fix typo

--- src/entries/20220710/index.tsx
+++ src/entries/20220710/index.tsx
@@ -7,7 +7,7 @@ export const Entry20220710 = () => <Entry
     width: 800,
     src: `${Config.staticPrefix}/9044.jpg`
   }}
-  caption={'I used to by comics here, Sydney 2022'}
+  caption={'I used to buy comics here, Sydney 2022'}
 />
 
 export default Entry20220710
