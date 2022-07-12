30c7964b2e0bd1587ead960ce88d6b8c41b441ff
Alex B <alexb@thenationofalex.com>
2022-07-12 21:27:49 +1000

    new page

---
+++ src/entries/20220712/index.tsx
@@ -0,0 +1,12 @@
+import Entry from '../../components/entry'
+import Config from '../../config'
+
+export const Entry20220712 = () => <Entry
+  image={{ 
+    alt: '',
+    width: 800,
+    src: `${Config.staticPrefix}/QmUypZecDTyLRqJ4THrbcdL4ncuVMTwfoQZLXKd9gJKLsP.jpeg`
+  }}
+  caption={'NFT'}
+/>
+
