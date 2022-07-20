6064a7081d1297f49ef4abecdb4ac8601ede7e11
Alex B <alexb@thenationofalex.com>
2022-07-21 00:32:33 +1000

    Deploy

---
+++ out/_next/static/3GGshTW-kP45mc1L2CVqS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e043b2e97d12b595.css","static/chunks/pages/index-6dd7baa94e7598a4.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- out/_next/static/OWRscWl-fbHkQ8sl_tjbC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/75cd51a22f9fe8be.css","static/chunks/pages/index-1daa0ddc220c55b4.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
