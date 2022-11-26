7f53b33a7aeb20b3c28be0e67bf07d36c5e8a67e
Alex B <alexb@thenationofalex.com>
2022-11-26 17:49:32 +1100

    new page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import { Entry20220824 as Entry } from '../src/entries/20220824'
+import { Entry20221126 as Entry } from '../src/entries/20221126'
 
 const Home: NextPage = () => {
   return (
---
+++ src/entries/20221126/index.tsx
@@ -0,0 +1,12 @@
+import Entry from '../../components/entry'
+import Config from '../../config'
+
+export const Entry20221126 = () => <Entry
+  image={{ 
+    alt: '',
+    width: 800,
+    src: `${Config.staticPrefix}/IMG_9843.JPG`
+  }}
+  caption={'Skeletal Remains'}
+/>
+
